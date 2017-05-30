830f8efed4c3c3a59112fa7832fe6b73add052d4
LENOVO-PC\Yang <yangemail8@gmail.com>
2017-05-29 17:49:43 -0700

    Finish sample 7.7

--- public/application.js
+++ public/application.js
@@ -1,1 +1,13 @@
+var mainApplicationModuleName = 'mean';
 
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'example']);
+// var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']);
+
+// Hashbangs support
+mainApplicationModule.config(['$locationProvider', function ($locationProvider) {
+    $locationProvider.hashPrefix('!');
+}]);
+
+angular.element(document).ready(function () {
+    angular.bootstrap(document, [mainApplicationModuleName]);
+});
